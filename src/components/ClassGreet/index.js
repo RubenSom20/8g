adece157aca69d4ac5f9c4b7e921fe71d0b9bb96
Ruben Somuano <rubasomuano.g2@gmail.com>
2020-08-05 18:26:08 -0500

    cambios errores

---
+++ src/components/ClassGreet/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class ClassGreet extends Component{
+  render() {
+    return <p>Hola {this.props.firstName}{this.props.lastName}</p>
+}
+}
+
+export default ClassGreet
